0b0ee50cd5fc590e8ccb1ccdd6a9465cf08ee3ef
Hanson <shadowguy369@gmail.com>
2020-06-20 16:48:10 -0700

    test

--- ScheduleBuilder/src/Student.java
+++ ScheduleBuilder/src/Student.java
@@ -1,24 +1,10 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
 
 public class Student {
 
-	
-	
-	public Student() throws FileNotFoundException {
+	public Student() {
 		
-		BufferedReader br = new BufferedReader(new FileReader("src\\StudentData.txt"));
-		Scanner s = new Scanner(System.in);
-		
-		while(s.hasNextLine()) {
-			
-			
-			
+
 		}
 		
-		
-	}
 
 }
