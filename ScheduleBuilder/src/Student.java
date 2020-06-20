993c29229acd42765047cd8c914c9a452c3141c2
Hanson <shadowguy369@gmail.com>
2020-06-20 16:46:42 -0700

    added classes

--- ScheduleBuilder/src/Student.java
+++ ScheduleBuilder/src/Student.java
@@ -1,5 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class Student {
 
+	
+	
+	public Student() throws FileNotFoundException {
+		
+		BufferedReader br = new BufferedReader(new FileReader("src\\StudentData.txt"));
+		Scanner s = new Scanner(System.in);
+		
+		while(s.hasNextLine()) {
+			
+			
+			
+		}
+		
+		
+	}
 
 }
