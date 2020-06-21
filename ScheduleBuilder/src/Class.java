8ef5b531b2f58719fb9b27a71e6f90e097453fd4
kidscomputer <kidscomputer@pauls-air.attlocal.net>
2020-06-20 19:57:03 -0700

    Updated Class

--- ScheduleBuilder/src/Class.java
+++ ScheduleBuilder/src/Class.java
@@ -1,4 +1,18 @@
+import java.util.*;
+
 
 public class Class {
 
+	private String subject;
+	private int period;	
+	private int capacity;
+	private ArrayList <Student> students = new ArrayList<Student>();
+	
+	public Class(String subject, String period, String capacity) {
+		
+		this.subject = subject;
+		this.period = Integer.parseInt(period);
+		this.capacity = Integer.parseInt(capacity);
+		students = null;
+	}
 }
