fbe5c4e9a165ee2639c481f9bb8c3af4042d8d7b
andyl <andyl@10.0.0.113>
2020-06-20 23:41:22 -0700

    test stuff for subject class

--- ScheduleBuilder/src/Subject.java
+++ ScheduleBuilder/src/Subject.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Subject {
@@ -6,10 +9,34 @@ public class Subject {
 	
 	public Subject(String subject) {
 		this.subject = subject;
-		createTeacherList();
-		System.out.println("test");
+		//createTeacherList();
 	}
 
+	/*
 	private void createTeacherList(){
+		BufferedReader br = new BufferedReader(new FileReader("src\\TeacherData.txt"));
+		String s = "";
+		ArrayList<String> lines = new ArrayList<String>();
+		
+		while(s!=null)
+		{
+			try 
+			{   //read each line into a String
+				s= br.readLine();
+				if(s!=null)  //add names to an ArrayList
+					lines.add(s);
+			} 
+			catch (IOException e) 
+			{
+				System.out.println("Something went wrong");
+			}
+		}
+		for(String x:lines)
+		{  
+			createTeacher(x);
+		}
 	}
+	*/
+	
+	
 }
